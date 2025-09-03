d9da9328bd8916d607dc97240452ac1314f32129
meow-codes201 <humairat291@gmail.com>
2025-09-03 20:11:03 +0530

    added images

--- library.py
+++ library.py
@@ -1,10 +1,21 @@
 from tkinter import *
 window = Tk() 
-window.geometry("420x420")
+window.geometry("520x520")
 window.title("My First GUI")
 libicon = PhotoImage(file='D:/libraryManager/libicon.png')
 window.iconphoto(True, libicon)
 # window.config(background="#9a8cdf")
-label = Label(window,text="Hello World!",font=("Times New Roman",20))
-label.pack(pady=20)
+photo = PhotoImage(file='D:/libraryManager/libicon.png')
+label = Label(window,text="CAN YOU SHUT UP AND ,LEARN",
+              font=("Times New Roman",20,'bold','underline'),
+              fg="#c3002a",bg="#9a8cdf",
+              padx=10,
+              pady=10,
+              relief=RAISED,
+              borderwidth=3,
+              bd=10,
+              image=photo,
+              compound='top')
+
+label.pack()
 window.mainloop()
